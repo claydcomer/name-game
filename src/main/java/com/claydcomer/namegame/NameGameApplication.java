03884d9327aef9d44e798a15aac26d37c819dc7b
Clay Comer <claydcomer@gmail.com>
2021-12-27 14:25:52 -0500

    Successfully retrieve response from willowtree url

--- src/main/java/com/claydcomer/namegame/NameGameApplication.java
+++ src/main/java/com/claydcomer/namegame/NameGameApplication.java
@@ -1,21 +1,15 @@
 package com.claydcomer.namegame;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class NameGameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NameGameApplication.class, args);
 	}
-
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
 }
--- src/main/java/com/claydcomer/namegame/service/ProfileService.java
+++ src/main/java/com/claydcomer/namegame/service/ProfileService.java
@@ -4,27 +4,31 @@ import com.claydcomer.namegame.model.Profile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProfileService {
 
-    private final RestTemplate restTemplate;
-
     @Value( "${willowtree.profiles.url}" )
     private String profileUrl;
 
-    @Autowired
-    public ProfileService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
     public List<Profile> getProfiles() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN));
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(converter);
+
+
         ResponseEntity<List<Profile>> response = restTemplate.exchange(
                 profileUrl,
                 HttpMethod.GET,
