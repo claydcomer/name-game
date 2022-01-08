16a06825da864bc15e0a0d2195c7f0284aa5ad4a
Clay Comer <claydcomer@gmail.com>
2022-01-07 23:56:47 -0500

    Update api doc and variable naming

--- src/main/java/com/claydcomer/namegame/controller/GameController.java
+++ src/main/java/com/claydcomer/namegame/controller/GameController.java
@@ -5,10 +5,7 @@ import com.claydcomer.namegame.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/game")
@@ -21,8 +18,8 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    @PostMapping("/start")
-    public ResponseEntity<Game> startGame(@RequestParam("type") String gameType) {
+    @PostMapping("/start/{type}")
+    public ResponseEntity<Game> startGame(@PathVariable("type") String gameType) {
         Game game = gameService.startGame(gameType);
 
         return new ResponseEntity<>(game, HttpStatus.CREATED);
